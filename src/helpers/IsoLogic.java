fe66be5514a96a71f504d66994d9151c75d3dbc8
raynerhayl <Haylem@192.168.20.10>
2015-09-24 15:18:33 +1200

    Modified the isoLogic class
    Changed it so that you can access just the components of a x/y point in
    terms of the isometric axis. Also added more documentation

--- src/helpers/IsoLogic.java
+++ src/helpers/IsoLogic.java
@@ -1,5 +1,7 @@
 package helpers;
 
+import java.awt.Point;
+
 /**
  * A class which helps with the isometric math.
  * 
@@ -29,25 +31,113 @@ public class IsoLogic {
 	 * isometric axis.
 	 * 
 	 * @param x
-	 *            the x position on the screen
+	 *            - the x position on the screen
 	 * @param y
-	 *            the y position on the screen
+	 *            - the y position on the screen
 	 * @param oX
-	 *            the x position of the bottom corner of the iso axis
+	 *            - the x position of the bottom corner of the iso axis
 	 * @param oY
-	 *            the y position of the bottom corner of the iso axis
+	 *            - the y position of the bottom corner of the iso axis
 	 * @return
 	 */
 	public int[] computeGridPos(int x, int y, int oX, int oY) {
-		int[] gridPos = new int[2];
+		double[] axisPos = computeAxisPos(x, y, oX, oY);
+		return new int[] { (int) (axisPos[0] / (axisPos[0] - axisPos[0] % this.xLength)), (int) (axisPos[0] / (axisPos[0] - axisPos[0] % this.xLength)) };
+	}
 
-		double yLength = (oY - y) / Math.sin(yAngle);
-		double xLength = (oX - x) * Math.sin(xAngle);
+	/**
+	 * Gives the position on the isometric axis in terms of the two axis.
+	 * 
+	 * @param x
+	 *            - The horizontal position on the x/y axis (the screen)
+	 * @param y
+	 *            - The vertical position on the x/y axis (the screen)
+	 * @param oX
+	 *            - The x coordinate of the isometric axis origin (the bottom
+	 *            corner)
+	 * @param oY
+	 *            - The y coordinate of the isometric axis origin (the bottom
+	 *            corner)
+	 * @return an array of doubles containing the coordinates of this x/y point
+	 *         on the isometric axis. The first value gives the coordinate on
+	 *         the right hand side axis of the isometric axis (furtherest to the
+	 *         right) and the second value gives the coordinate on the axis
+	 *         furtherest to the left.
+	 */
+	private double[] computeAxisPos(int x, int y, int oX, int oY) {
+		return new double[] { xLength = (oX - x) * Math.sin(xAngle), (oY - y) / Math.sin(yAngle) };
+	}
 
-		gridPos[0] = (int) (xLength / (xLength - xLength % this.xLength));
-		gridPos[1] = (int) (yLength / (yLength - yLength % this.yLength));
+	/**
+	 * Gives the component of this point on the U axis. The U axis is the
+	 * isometric axis which is CLOSEST to the Y axis of the screen. The x/y
+	 * coordinates of the isometric axis origin must be given. The origin is
+	 * defined as the bottom corner of our isometric square plane.
+	 * 
+	 * @param x
+	 *            - The x coordinate of the point.
+	 * @param y
+	 *            - The y coordinate of the point.
+	 * @param oX
+	 *            - The x coordinate of the isometric axis origin at the time of
+	 *            this calculation.
+	 * @param oY
+	 *            - The y coordinate of the isometric axis origin at the time of
+	 *            this calculation.
+	 * @return The value of this point along the U axis.
+	 */
+	public double isometricAxisU(int x, int y, int oX, int oY) {
+		return computeAxisPos(x, y, oX, oY)[1];
+	}
 
-		return gridPos;
+	/**
+	 * Point parameter alternative to the method
+	 * {@link #isometricAxisU(int, int, int, int)}.
+	 * 
+	 * @param p
+	 *            - The point in terms of the x/y axis of the screen.
+	 * @param o
+	 *            - The point of the isometric axis origin on the screen.
+	 * @return
+	 */
+	public double isometricAxisU(Point p, Point o) {
+		return computeAxisPos(p.x, p.y, o.x, o.y)[1];
+	}
+
+	/**
+	 * Gives the component of this point on the V axis. The V axis is the
+	 * isometric axis which is FURTHEST to the Y axis of the screen. The x/y
+	 * coordinates of the isometric axis origin must be given. The origin is
+	 * defined as the bottom corner of our isometric square plane.
+	 * 
+	 * @param x
+	 *            - The x coordinate of the point.
+	 * @param y
+	 *            - The y coordinate of the point.
+	 * @param oX
+	 *            - The x coordinate of the isometric axis origin at the time of
+	 *            this calculation.
+	 * @param oY
+	 *            - The y coordinate of the isometric axis origin at the time of
+	 *            this calculation.
+	 * @return The value of this point along the V axis.
+	 */
+	public double getV(int x, int y, int oX, int oY) {
+		return computeAxisPos(x, y, oX, oY)[0];
+	}
+
+	/**
+	 * Point parameter alternative to the method
+	 * {@link #isometricAxisU(int, int, int, int)}.
+	 * 
+	 * @param p
+	 *            - The point in terms of the x/y axis of the screen.
+	 * @param o
+	 *            - The point of the isometric axis origin on the screen.
+	 * @return
+	 */
+	public double getV(Point p, Point o) {
+		return computeAxisPos(p.x, p.y, o.x, o.y)[0];
 	}
 
 	public void setyAngle(double yAngle) {
